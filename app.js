e70d0fdbe24715fbbe1ee4c0fff8b178308e9219
Nikhil Kumar <nknapster@gmail.com>
2021-10-27 00:12:24 +0530

    ok

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const express = require("express");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
+const blogData = require("./blogData.js")
 
 const app = express();
-//app.use(express.json);
+
 app.use(express.urlencoded({extended:true}));
 
 
@@ -20,22 +21,13 @@ const Article = mongoose.model('articles',articleSchema);
 
 
 
-const homeStartingContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-var contactContent= "this";
-var aboutContent = "this is about page"
-
-
-
-
-
-
-
 
 app.get('/',(req,res)=>{
+  //
   Article.find({},async(err,result)=>{
     if(!err){
       posts = await result;
-      res.render("home.ejs",{startingContent:homeStartingContent,posts: posts});
+      res.render("home.ejs",{startingContent:blogData.homecontent,posts: posts});
     }else{
       console.log(err);
     }
@@ -45,27 +37,33 @@ app.get('/',(req,res)=>{
 });
 
 app.get('/contact',(req,res)=>{
-  res.render("contact.ejs",{content:contactContent});
+  res.render("contact.ejs",{content:blogData.contactContent});
 });
 
 app.get('/about',(req,res)=>{
-  res.render("about.ejs",{content:aboutContent});
+  res.render("about.ejs",{content:blogData.aboutContent});
 });
 
 app.get("/compose",(req,res)=>{
   res.render("compose.ejs")
 });
 
-app.post("/compose",(req,res)=>{
-  const post = {
+app.post("/compose",async (req,res)=>{
+  console.log(req.body);
+  const post = new Article ({
     title:req.body.postTitle,
     content:req.body.postBody
-  };
-  posts.push(post);
+  });
+  await post.save();
   res.redirect("/");
 });
 
+
 app.get('/posts/:post',(req,res)=>{
+  //a route to all indivisual posts dynamicaly created pages
+  //ist a ancher tag with a href to /post/ ejs (post.tilte)
+  // get the post tilte by taping into req.prams and then go through lowdash
+  //
   posts.forEach(post => 
     {
       if  (_.lowerCase( post.title) == _.lowerCase(req.params.post))
